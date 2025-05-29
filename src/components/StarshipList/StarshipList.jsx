1e1401d957f0478a631f9cda9c0dc86c1ef579a5
Jacob Tham <jacobtham3@gmail.com>
2025-05-29 10:28:42 +0800

    reverted back to before I tried using GPT as a last resort

--- src/components/StarshipList/StarshipList.jsx
+++ src/components/StarshipList/StarshipList.jsx
@@ -1,23 +1,11 @@
-// src/components/StarshipList/StarshipList.jsx
-// import { StarshipCard } from "../StarshipCard/StarshipCard"; // Keep if you use it
+import { StarshipCard } from "../StarshipCard/StarshipCard";
 
 const StarshipList = ({ starships }) => {
-  // console.log("Starships received in StarshipList:", starships); // For debugging
-
-  if (!starships || starships.length === 0) {
-    return <p>No starships to display.</p>;
-  }
-
   return (
-    <div>
-      <h2>All Starships</h2> {/* Changed title for clarity */}
+    <>
       <ul>
         {starships.map((starship) => (
           <li key={starship.name}>
-            {/* If using StarshipCard, uncomment and pass the starship prop */}
-            {/* <StarshipCard starship={starship} /> */}
-
-            {/* Otherwise, render directly */}
             <h3>Name: {starship.name}</h3>
             <p>Manufacturer: {starship.manufacturer}</p>
             <p>Class: {starship.class}</p>
@@ -25,7 +13,7 @@ const StarshipList = ({ starships }) => {
           </li>
         ))}
       </ul>
-    </div>
+    </>
   );
 };
 
